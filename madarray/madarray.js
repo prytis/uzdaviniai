19c9bdf5313687a0147cc2d97877a54ba3ad11ab
= <=>
2020-05-05 14:18:42 +0300

    mad array

---
+++ madarray/madarray.js
@@ -0,0 +1,18 @@
+"use strict";
+function madArray (a,b) {
+    let squareSum = 0;
+    let cubeSum = 0;
+    for ( let i = 0; i < a.length; i++){
+        squareSum = squareSum + a[i]*a[i];
+    }
+    for ( let i = 0; i < b.length; i++){
+        cubeSum = cubeSum + b[i]*b[i]*b[i];
+    }
+    console.log( squareSum+' '+cubeSum);
+    if (squareSum > cubeSum){
+        return true;
+    } return false;
+console.log( squareSum+' '+cubeSum);
+}
+
+console.log(madArray([4,5,6],[1,2,3,9]));
